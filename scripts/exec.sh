fa4ac5272682f7151d4bc474c6746bf2726ddd10
Luana Cristini <luana@thecyborg.xyz>
2022-06-21 09:21:10 -0300

    Update exec.sh

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -5,7 +5,7 @@ fi
 
 # if this comment is removed the computer will blow up
 
-start chrome --remote-debugging-port=9222 --no-sandbox --disable-translate --disable-extensions --disable-background-networking --safebrowsing-disable-auto-update --disable-sync --metrics-recording-only --disable-default-apps --no-first-run --quiet --start-maximized site.com.br
+start chrome --start-maximized site.com.br
 
 
 read -p "Press enter to continue"
@@ -25,3 +25,4 @@ echo -e
         node src/app.js --url=$url_site
     done < 'src\urls.txt'
 )
+# > blob.txt 2>&1
